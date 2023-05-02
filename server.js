c9e58e78000f341b5725ee70b0710ebf567a0e56
kopham <khoi.2.pham@ucdenver.edu>
2023-05-01 21:42:11 -0600

    new changes

--- server.js
+++ server.js
@@ -13,37 +13,57 @@ app.use(express.json())
 app.use(Cors())
 
 //DB Config
-
 mongoose.connect(connection_url, {
     useNewUrlParser: true,
     //useCreateIndex: true,
     useUnifiedTopology: true
 })
 
-
 //API Endpoints
 app.get("/", (req, res) => res.status(200).send("Hello me"))
 
+
+// New 
 app.post('/dating/cards', (req, res) => {
     const dbCard = req.body
-    Cards.create(dbCard, (err, data) => {
-        if(err) {
-            res.status(500).send(err)
-        } else {
-            res.status(201).send(data)
-        }
+    Cards.create(dbCard).then((data) => {
+    console.log(JSON.stringify(data))
+    res.status(200).send(data)
     })
 })
 
 app.get('/dating/cards', (req, res) => {
-        Cards.find((err, data) => {
-        if(err) {
-            res.status(500).send(err)
-        } else {
-            res.status(200).send(data)
-        }
+    Cards.find().then((data) => {
+    res.status(200).send(data)
     })
 })
+
+/*
+app.get('/dating/cards', (req, res) => {
+    Cards.find({})
+    .then(data => {
+    res.status(201).send(data);
+    })
+    .catch(err => {
+    console.error(err);
+    res.status(500).send({ error: 'An error occurred while reading the card' });
+    });
+});
+
+app.post('/dating/cards', (req, res) => {
+    const dbCard = req.body;
+    Cards.create(dbCard)
+    .then(data => {
+    res.status(201).send(data);
+    })
+    .catch(err => {
+    console.error(err);
+    res.status(500).send({ error: 'An error occurred while creating the card' });
+    });
+});
+*/
+
+    
 
 //Listener
 app.listen(port, () => console.log(`Listening on localhost: ${port}`))
